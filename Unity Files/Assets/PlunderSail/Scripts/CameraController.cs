2adc858a96cc60dda81b966e4ecf0599de9ede76
Mario Barone <mario.e.barone@gmail.com>
2017-10-10 23:35:20 +1100

    Some general cleanup: deleted scratch scene renamed some oasis prefabs added some new island prefabs created ZONE prefabs (for grid puzzle map thing) created test scene for making the ZONE prefabs. created some clean material folders Imported palm textures and replaced cannon textures
    Moved CameraController and Pier into Scripts Folder

---
+++ Unity Files/Assets/PlunderSail/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buildCamGO;
+
+    [SerializeField]
+    private GameObject playCamGO;
+
+    private BuildModeCam buildCam;
+
+    private PlayModeCam playCam;
+
+    private void Awake()
+    {
+        // Subscribe to game state
+        GameState.buildModeChanged += SetBuildMode;
+
+        playCam = playCamGO.GetComponentInChildren<PlayModeCam>();
+        buildCam = buildCamGO.GetComponentInChildren<BuildModeCam>();
+    }
+
+    private void SetBuildMode(bool isBuildMode)
+    {
+        playCamGO.SetActive(!isBuildMode);
+        buildCamGO.SetActive(isBuildMode);
+        buildCam.UpdatePos();
+    }
+
+    public BuildModeCam BuildCam
+    {
+        get { return buildCam; }
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe to game state
+        GameState.buildModeChanged -= SetBuildMode;
+    }
+}
---
+++ Unity Files/Assets/PlunderSail/Scripts/Pier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pier : MonoBehaviour
+{
+    [SerializeField]
+    private Transform dockingPos;
+
+    private UIManager UI;
+    private GameManager gm;
+
+    private void Awake()
+    {
+        UI = GameObject.FindGameObjectWithTag("GameManager").GetComponent<UIManager>();
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            UI.ShowPierPopUp(true);
+            gm.setPier(dockingPos);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UI.ShowPierPopUp(false);
+            gm.setPier(null);
+        }
+    }
+}

e592850c231be5e53f6981a8f4a2e852c308d855
AaronDSmit <smitaaron1@gmail.com>
2017-10-11 12:35:08 +1100

    First pass on map maker

--- Unity Files/Assets/PlunderSail/Scripts/MapManager.cs
+++ Unity Files/Assets/PlunderSail/Scripts/MapManager.cs
@@ -6,7 +6,15 @@ public class MapManager : MonoBehaviour
 {
     private Transform player;
 
-    private Transform[,] zones;
+    private Zone[,] map;
+
+    [SerializeField]
+    private GameObject[] zones;
+
+    [SerializeField]
+    private GameObject waterTile;
+
+    private Vector3[] SpawnPoints;
 
     private void Awake()
     {
@@ -15,11 +23,43 @@ public class MapManager : MonoBehaviour
 
     private void Start()
     {
-        zones = new Transform[5,5];
+        map = new Zone[5, 5];
     }
 
     private void InitMap()
     {
+        foreach (Zone zone in map)
+        {
+            zone.Init(waterTile, zones[Random.Range(0, zones.Length)]);
+        }
+    }
+}
 
+public class Zone : MonoBehaviour
+{
+    private GameObject waterTile;
+    private GameObject islandsPrefab;
+
+    private Vector3 pos;
+
+    public void Init(GameObject _water, GameObject _islands)
+    {
+        waterTile = _water;
+        islandsPrefab = _islands;
+    }
+
+    public void Spawn(Vector3 _spawnPos)
+    {
+        Transform water = Instantiate(waterTile, _spawnPos, Quaternion.identity).transform;
+        water.parent = transform;
+
+        Transform islands = Instantiate(waterTile, _spawnPos, Quaternion.identity).transform;
+        islands.parent = transform;
+    }
+
+    public Vector3 Position
+    {
+        get { return pos; }
+        set { pos = value; }
     }
 }

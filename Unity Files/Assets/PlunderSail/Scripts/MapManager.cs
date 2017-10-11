508e984e54bec3fd292140fd187fe8bd64cf70ee
AaronDSmit <smitaaron1@gmail.com>
2017-10-11 20:52:06 +1100

    Removed CameraMovement.cs, CameraOrbit.cs, EndlessWaterSquare.cs, WaterController.cs, WaterSquare.cs. Moved Camera rig prefabs into folder

--- Unity Files/Assets/PlunderSail/Scripts/MapManager.cs
+++ Unity Files/Assets/PlunderSail/Scripts/MapManager.cs
@@ -4,62 +4,41 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    private Transform player;
-
     private Zone[,] map;
 
     [SerializeField]
-    private GameObject[] zones;
-
-    [SerializeField]
-    private GameObject waterTile;
+    private GameObject[] zonePrefabs;
 
     private Vector3[] SpawnPoints;
 
-    private void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-
     private void Start()
     {
-        map = new Zone[5, 5];
+        map = new Zone[9, 9];
+
+        //InitMap();
     }
 
+    // Loops through the 2D array of Zones and instantiates the prefabs, sets to null if outside of the diamond map shape.
     private void InitMap()
     {
-        foreach (Zone zone in map)
+        // Used to tapper off the top and bottom of the 2D grid to create a diamon shape within it.
+        int killZone = 3;
+
+        for (int y = 0; y < 9; y++)
         {
-            zone.Init(waterTile, zones[Random.Range(0, zones.Length)]);
+            for (int x = 0; x < 9; x++)
+            {
+                if (x > killZone && x <= map.GetLength(0) - killZone)
+                {
+                    map[x, y] = Instantiate(zonePrefabs[Random.Range(0, zonePrefabs.Length)], new Vector3(x, 0, y), Quaternion.identity).GetComponent<Zone>();
+                    killZone--;
+                }
+                else
+                {
+                    map[x, y] = null;
+                }
+            }
         }
-    }
-}
 
-public class Zone : MonoBehaviour
-{
-    private GameObject waterTile;
-    private GameObject islandsPrefab;
-
-    private Vector3 pos;
-
-    public void Init(GameObject _water, GameObject _islands)
-    {
-        waterTile = _water;
-        islandsPrefab = _islands;
-    }
-
-    public void Spawn(Vector3 _spawnPos)
-    {
-        Transform water = Instantiate(waterTile, _spawnPos, Quaternion.identity).transform;
-        water.parent = transform;
-
-        Transform islands = Instantiate(waterTile, _spawnPos, Quaternion.identity).transform;
-        islands.parent = transform;
-    }
-
-    public Vector3 Position
-    {
-        get { return pos; }
-        set { pos = value; }
     }
 }
---
+++ Unity Files/Assets/PlunderSail/Scripts/Zone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone : MonoBehaviour
+{
+    private enum Tier { MINOR, MODERATE, ELITE }
+
+    private enum TYPE { SCARCE }
+
+    [Header("Zone properties")]
+    [SerializeField]
+    private Tier tier = Tier.MINOR;
+
+    [SerializeField]
+    private TYPE zoneType = TYPE.SCARCE;
+
+    [Header("Zone contents")]
+    [SerializeField]
+    private bool hasDock = false;
+
+    [SerializeField]
+    private bool hasTower = false;
+}

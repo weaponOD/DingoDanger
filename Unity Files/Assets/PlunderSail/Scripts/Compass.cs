c7334448476c5fea493b2d79df8de677ad6d211b
Mario Barone <mario.e.barone@gmail.com>
2017-10-23 16:20:25 +1100

    Updated Compass Added Compass icons Updated Environment Meshes Adjusted Build UI layout

---
+++ Unity Files/Assets/PlunderSail/Scripts/Compass.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour {
+
+	[SerializeField]
+	private Vector3 northDir;
+
+	[SerializeField]
+	private Transform player;
+
+	[SerializeField]
+	private Quaternion objectiveDir;
+
+	[SerializeField]
+	public RectTransform northArrow;
+
+	[SerializeField]
+	public RectTransform objectiveArrow;
+
+	[SerializeField]
+	public Transform objectiveDestination;
+	
+
+	private void Update () {
+		ChangeNorthDirection ();
+		ChangeObjectiveDirection ();
+	}
+
+	public void ChangeNorthDirection(){
+		northDir.z = player.eulerAngles.y;
+
+		northArrow.localEulerAngles = northDir;
+	}
+
+	public void ChangeObjectiveDirection(){
+		
+		Vector3 dir = transform.position - objectiveDestination.position;
+
+		objectiveDir = Quaternion.LookRotation (dir);
+
+		objectiveDir.z = -objectiveDir.y;
+
+		objectiveDir.x = 0;
+
+		objectiveDir.y = 0;
+
+		objectiveArrow.localRotation = objectiveDir * Quaternion.Euler (northDir);
+	}
+}

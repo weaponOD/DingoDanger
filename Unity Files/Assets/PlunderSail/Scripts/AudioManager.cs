d13b9d73b5a7a1971f80351522e5ccf2b066e4e3
Mario Barone <mario.e.barone@gmail.com>
2017-11-09 21:55:11 +1100

    Made Additions to Build UI -Removed Set Sail from buttons, and made solo button (based on feedback) -Added quick animation to Set Sail button (confirmation image)
    Moved Battle Music 
    
    Added Audio Manager and SoundClass (used to assign audio sources)
    -Created AudioManager Prefab with oneshot sound effects pre-assigned
    
    Modified Enemies colliders and stats, Player damage stats
    
    Added new Weapon UI for current weapon and reload bars for both left and right per weapon type (MOCKUP)

---
+++ Unity Files/Assets/PlunderSail/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+//List of sounds that we can add or remove as we go, each sound will have options to adjust volume, pitch etc. 
+//During Start of game we go through list of sounds and find one we need, and call Play Fiunction to play sounds in other scripts
+
+
+//Call sound effects using this line				AudioManager.instance.Play("soundClassname here");
+
+public class AudioManager : MonoBehaviour {
+
+	[Tooltip("Add sounds to this Array, their Name will be used to call them.")]
+	//Array of Sounds we can access
+	public SoundClass [] sounds;
+
+	//Static ref to audio manager
+	public static AudioManager instance;
+
+
+
+	//Add a audio source to each clip at the beginning of the game.
+	void Awake () {
+
+		if(instance == null)
+			instance = this;
+		else
+		{
+			//stop the game from having more than one audio manager
+			Destroy(gameObject);
+			return;
+		}
+
+
+		//Don't destroy the Audio Manager in scene
+		DontDestroyOnLoad (gameObject);
+
+		foreach (SoundClass _sounds in sounds) {
+			_sounds.audioSource =  gameObject.AddComponent<AudioSource> ();
+			_sounds.audioSource.clip = _sounds.audioClip;
+
+			_sounds.audioSource.volume = _sounds.volume;
+			_sounds.audioSource.pitch = _sounds.pitch;
+			_sounds.audioSource.loop = _sounds.looping;
+		}
+	}
+
+
+	//Call this function through other script
+	//e.g AudioManager.instance.Play("CannonImpact");
+	public void PlaySound(string _name){
+		SoundClass soundClass = Array.Find (sounds, sound => sound.name == _name);
+		if (soundClass == null){
+			Debug.LogWarning ("Sound: " + _name + " not found.");
+			return;
+		}
+			
+
+		soundClass.audioSource.Play ();
+	}
+}
---
+++ Unity Files/Assets/PlunderSail/Scripts/SoundClass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundClass{
+
+	public string name;
+
+	public AudioClip audioClip;
+
+	[Range(0f, 1f)]
+	public float volume = 1f;
+
+	[Range(0.5f, 2f)]
+	public float pitch = 1f;
+
+	public bool looping;
+
+	[HideInInspector]
+	public AudioSource audioSource;
+
+
+}
